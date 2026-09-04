fb0046d76e794284671240a79200de67ceb831a2
msx98 <git@lx.ax>
2026-09-04 10:36:05 +0300

    build 2026-09-04 10:36:05

--- iPipe/Features/VideoDetail/VideoDetailView.swift
+++ iPipe/Features/VideoDetail/VideoDetailView.swift
@@ -300,7 +300,9 @@ struct VideoDetailView: View {
     private func setOrientation(_ orientation: UIInterfaceOrientation) {
         guard let scene = UIApplication.shared.connectedScenes.compactMap({ $0 as? UIWindowScene }).first else { return }
         let mask: UIInterfaceOrientationMask = orientation.isLandscape ? .landscape : .portrait
-        scene.requestGeometryUpdate(.iOS(interfaceOrientations: mask)) { error in
+        do {
+            try scene.requestGeometryUpdate(.iOS(interfaceOrientations: mask))
+        } catch {
             NSLog("iPipe: orientation request failed: %@", error.localizedDescription)
         }
         scene.windows.first?.rootViewController?.setNeedsUpdateOfSupportedInterfaceOrientations()
